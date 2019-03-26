b7f7ecb6553e1de5b563a30ce611a0d17bbc5dc0
Richard Afro <richyafro@hotmail.com>
2019-03-26 17:58:32 +0100

    Install app plugins

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import Vue from "vue";
+import VSocket from "vue-socket.io";
+import { ChatInstaller } from "@progress/kendo-chat-vue-wrapper";
+import "@progress/kendo-ui";
+import "@progress/kendo-theme-default/dist/all.css";
+
 import App from "./App.vue";
+
+Vue.use(ChatInstaller);
+Vue.use(
+  new VSocket({
+    debug: true,
+    connection: "http://localhost:4000"
+  })
+);
 
 Vue.config.productionTip = false;
 
