5e9af35f3179d0ed1c574f73a63f70061c36d327
Richard Afro <richyafro@hotmail.com>
2019-03-26 17:58:24 +0100

    App base file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header />
+    <Chat/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Chat from "./components/Chat";
+import Header from './components/Header';
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Chat,
+    Header
   }
 };
 </script>
@@ -21,8 +23,8 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 70%;
+  margin: auto;
 }
 </style>
